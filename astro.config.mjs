9ed54317453ccddc411e85dce5faa3ec18754b74
Alexander Bagatka <alex.bagatka@gmail.com>
2025-06-20 18:25:08 +0200

    Update image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,16 @@ export default defineConfig(
   {
     site: 'https://bagatka.github.io',
     base: '/insd-shp/',
+    image: {
+      service: {
+        entrypoint: 'astro/assets/services/sharp',
+        config: {
+          quality: 95,
+          // Limit format conversion
+          limitInputPixels: false,
+        }
+      }
+    },
     vite: {
       // @ts-ignore
       plugins: [tailwindcss()],
